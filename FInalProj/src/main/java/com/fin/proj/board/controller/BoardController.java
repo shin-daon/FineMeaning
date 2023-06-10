b0dbf5a0edcb756c8da096d4169e35c8b3dd5db8
seewol <jeeneepark@naver.com>
2023-06-11 00:37:00 +0900

    finePeople views update

--- FInalProj/src/main/java/com/fin/proj/board/controller/BoardController.java
+++ FInalProj/src/main/java/com/fin/proj/board/controller/BoardController.java
@@ -20,4 +20,14 @@ public class BoardController {
 	public String faqDetail() {
 		return "faq_detail";
 	}
+	
+	@GetMapping("finePeopleMain.bo")
+	public String finePeopleMain() {
+		return "finePeople";
+	}
+	
+	@GetMapping("finePeople_form.bo")
+	public String finePeopleForm() {
+		return "finePeople_form";
+	}
 }
