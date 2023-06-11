8cc699addb3fd46291c087b9f4ea287862171742
danbi7184 <danbi7184@gmail.com>
2023-06-12 00:41:28 +0900

    봉사 화면 헤더 푸터 추가, 컨트롤러 수정(페이지 연결만)

--- FInalProj/src/main/java/com/fin/proj/volunteer/controller/VolunteerController.java
+++ FInalProj/src/main/java/com/fin/proj/volunteer/controller/VolunteerController.java
@@ -2,6 +2,7 @@ package com.fin.proj.volunteer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.fin.proj.volunteer.model.service.VolunteerService;
 
@@ -10,4 +11,40 @@ public class VolunteerController {
 	
 	@Autowired
 	private VolunteerService vService;
+	
+	@GetMapping("volunteer.vo")
+	public String volunteer() {
+		return "volunteer";
+	}
+	
+	@GetMapping("volunteerDetail.vo")
+	public String volunteerDetail() {
+		return "volunteerDetail";
+	}
+	
+	@GetMapping("volunteerApply.vo")
+	public String volunteerApply() {
+		return "volunteerApply";
+	}
+	
+	@GetMapping("volunteerHistory.vo")
+	public String volunteerHistory() {
+		return "volunteerHistory";
+	}
+	
+	// 관리자
+	@GetMapping("volunteerEnroll.vo")
+	public String volunteerEnroll() {
+		return "volunteerEnroll";
+	}
+	
+	@GetMapping("volunteerEnrollHistory.vo")
+	public String volunteerEnrollHistory() {
+		return "volunteerEnrollHistory";
+	}
+	
+	@GetMapping("volunteerEdit.vo")
+	public String volunteerEdit() {
+		return "volunteerEdit";
+	}
 }
