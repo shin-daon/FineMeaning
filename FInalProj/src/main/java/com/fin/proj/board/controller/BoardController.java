76fdcbe2d571b926cd1eed139cfe1fb396350b60
rlgnszzzz <rlgnsz@naver.com>
2023-06-14 14:50:05 +0900

    일반게시판 화면 수정

--- FInalProj/src/main/java/com/fin/proj/board/controller/BoardController.java
+++ FInalProj/src/main/java/com/fin/proj/board/controller/BoardController.java
@@ -1,14 +1,87 @@
 package com.fin.proj.board.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.fin.proj.board.exception.BoardException;
+import com.fin.proj.board.model.service.BoardService;
+import com.fin.proj.board.model.vo.Board;
+import com.fin.proj.board.model.vo.Reply;
+import com.fin.proj.common.Pagination;
+import com.fin.proj.common.model.vo.PageInfo;
+import com.fin.proj.member.model.vo.Member;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class BoardController {
+	
+	@Autowired
+	private BoardService bService;
 
 	@GetMapping("faqMain.bo")
-	public String faqMain() {
-		return "faq";
+	public String faqMain(@RequestParam(value="page", required=false) Integer currentPage, Model model) {
+		
+		if(currentPage == null) {
+			currentPage = 1;
+		}
+		
+		int listCount = bService.getListCount(6);
+//		System.out.println(listCount);
+		
+		PageInfo pageInfo= Pagination.getPageInfo(currentPage, listCount, 10);
+		
+		ArrayList<Board> list = bService.selectBoardList(pageInfo, 6);
+//		System.out.println(list);
+		
+		if(list != null) {
+			model.addAttribute("pi", pageInfo);
+			model.addAttribute("list", list);
+			return "faq";
+		} else {
+			throw new BoardException("게시글 목록 조회 실패");
+		}
+	}
+	
+	@GetMapping("faq_detail.bo")
+	public String faqDetail(@RequestParam("bNo") int bNo, @RequestParam("writer") String writer,
+							@RequestParam("page") int page, HttpSession session, Model model) {
+//		System.out.println(bNo + ", " + writer + ", "+ page);
+		// 상세보기의 경우 조회수 +1, but 내 글 클릭 시 조회수 +0 (로그인 유저 정보 가져와서 비교)
+		// 파라미터로 받은 page를 통해 목록으로 돌아갔을 시 원래 보던 페이지 노출
+		
+		Member m = (Member)session.getAttribute("loginUser");
+		System.out.println(m);
+		
+		String readerNickName = null;
+		if(m != null) {
+			readerNickName = m.getuNickName();
+		}
+		
+		boolean countYN = false;
+		if(!writer.equals(readerNickName)) {
+			countYN = true;
+		}
+		
+		Board board = bService.selectBoard(bNo, countYN);
+		System.out.println(board);
+		
+		ArrayList<Reply> replyList = bService.selectReply(bNo);
+		System.out.println(replyList);
+		
+		if(board != null) {
+			model.addAttribute("board", board);
+			model.addAttribute("page", page);
+//			model.addAttribute("replyList", replyList);
+			return "faq_Detail";
+		} else {
+			throw new BoardException("게시글 상세 조회 실패");
+		}
 	}
 	
 	@GetMapping("faq_form.bo")
@@ -16,10 +89,6 @@ public class BoardController {
 		return "faq_form";
 	}
 	
-	@GetMapping("faq_detail.bo")
-	public String faqDetail() {
-		return "faq_detail";
-	}
 	
 	@GetMapping("finePeopleMain.bo")
 	public String finePeopleMain() {
@@ -114,6 +183,18 @@ public class BoardController {
 	@GetMapping("editNotice.bo")
 	public String editNotice() {
 		return "editNotice";
+	}
+	
+	
+	// my page
+	@GetMapping("myBoard.bo")
+	public String myBoard() {
+		return "myBoard";
+	}
+	
+	@GetMapping("myReply.bo")
+	public String myReply() {
+		return "myReply";
 	}
 	
 }
