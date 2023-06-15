334c56a8c4aa0faf0d4e393bd730009dae562ddd
shin-daon <zzangdaon@gmail.com>
2023-06-15 17:30:52 +0900

    수정

--- FInalProj/src/main/java/com/fin/proj/member/model/service/EmailService.java
+++ FInalProj/src/main/java/com/fin/proj/member/model/service/EmailService.java
@@ -18,17 +18,7 @@ public class EmailService {
 	
 	private int authNumber;
 	 
-//	public void sendSimpleMessage(Email mailDto) {
-//		SimpleMailMessage message = new SimpleMailMessage();
-//	    message.setFrom("finemeaning.kh@gmail.com");
-//	    message.setTo(mailDto.getAddress());
-//	    message.setSubject(mailDto.getTitle());
-//	    message.setText(mailDto.getContent());
-//	    emailSender.send(message);
-//	}
-	
 	public void makeRandomNumber() {
-		// 난수의 범위 111111 ~ 999999 (6자리 난수)
 		Random r = new Random();
 		int checkNum = r.nextInt(888888) + 111111;
 		System.out.println("인증번호 : " + checkNum);
@@ -37,17 +27,6 @@ public class EmailService {
 
 	public int checkEmail(String emailAddress) {		
 		makeRandomNumber();
-//		SimpleMailMessage message = new SimpleMailMessage();
-//		
-//	    message.setFrom("finemeaning.kh@gmail.com");
-//	    message.setTo(emailAddress);
-//	    message.setSubject("선뜻입니다");
-//	    message.setText("인증번호는 " + authNumber + " 입니다");
-//	    
-//	    emailSender.send(message);
-//	    System.out.println("메세지 : " + message);
-//	    
-//	    return authNumber;
 		
 		String setFrom = "finemeaning.kh@gmail.com"; // email-config에 설정한 자신의 이메일 주소를 입력 
 		String toMail = emailAddress;
@@ -65,14 +44,13 @@ public class EmailService {
 	//이메일 전송 메소드
 	public void mailSend(String setFrom, String toMail, String title, String content) { 
 		MimeMessage message = emailSender.createMimeMessage();
-		// true 매개값을 전달하면 multipart 형식의 메세지 전달이 가능.문자 인코딩 설정도 가능하다.
 		try {
-			MimeMessageHelper helper = new MimeMessageHelper(message,true,"utf-8");
+			MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
 			helper.setFrom(setFrom);
 			helper.setTo(toMail);
 			helper.setSubject(title);
 			// true 전달 > html 형식으로 전송 , 작성하지 않으면 단순 텍스트로 전달.
-			helper.setText(content,true);
+			helper.setText(content, true);
 			emailSender.send(message);
 		} catch (MessagingException e) {
 			e.printStackTrace();
