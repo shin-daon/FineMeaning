384d64c3b766d3d7ecf48f96c3505db9cccb557b
unknown <sp9495@daum.net>
2023-06-21 11:30:23 +0900

    인터셉터 추가

--- FInalProj/src/main/java/com/fin/proj/common/config/WebMvcConfig.java
+++ FInalProj/src/main/java/com/fin/proj/common/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.fin.proj.common.interceptor.CheckAdminInterceptor;
 import com.fin.proj.common.interceptor.CheckLoginInterceptor;
 
 @Configuration
@@ -13,5 +14,17 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		registry.addInterceptor(new CheckLoginInterceptor())
 				.addPathPatterns("/*.su")
 				.excludePathPatterns("/supportMain.su");
+		
+		registry.addInterceptor(new CheckAdminInterceptor())
+				.addPathPatterns("/supportListAdmin.su",
+								 "/supportApplyListAdmin.su",
+								 "/applyDevision.su",
+								 "/supportDetailAdmin.su",
+								 "/updateApplyStatus.su",
+								 "/supportEndListAdmin.su",
+								 "/searchSupportListAdmin.su",
+								 "/supporterListEach.su",
+								 "/supporterListAdmin.su",
+								 "/searchEndList.su");
 	}
 }
