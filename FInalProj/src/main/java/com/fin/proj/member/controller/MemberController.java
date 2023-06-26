fc225b6a85596fa8fb31f13c367ec69f9ae1c53e
shin-daon <zzangdaon@gmail.com>
2023-06-26 16:39:34 +0900

    중간 커밋

--- FInalProj/src/main/java/com/fin/proj/member/controller/MemberController.java
+++ FInalProj/src/main/java/com/fin/proj/member/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
@@ -422,4 +423,3 @@ public class MemberController {
     	out.print(result + "," + randomNum);  	
 	}
 }
-
